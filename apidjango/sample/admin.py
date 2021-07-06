a3137bffd3c01194281e8ee6557da6daf7742cda
almblm <sind_blm@yahoo.co.jp>
2021-07-07 00:34:50 +0900

    change admin malti

--- apidjango/sample/admin.py
+++ apidjango/sample/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from sample.models import  SampleName
+from sample.models import SampleName
 
-admin.site.register(SampleName)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id','name','bookType')
+    list_display_links = ('name','bookType')
+
+admin.site.register(SampleName,BookAdmin)
