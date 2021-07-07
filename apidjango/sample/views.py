c0273a08036d0a8a3c3fe45d534d97c46a425891
almblm <sind_blm@yahoo.co.jp>
2021-07-08 03:48:44 +0900

    add filter

--- apidjango/sample/views.py
+++ apidjango/sample/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 import django_filters
-from rest_framework import viewsets,filters
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
 from sample.models import SampleName
 from sample.serializer import SampleSerializer
@@ -11,3 +12,5 @@ from sample.serializer import SampleSerializer
 class SampleViewSet(viewsets.ModelViewSet):
     queryset = SampleName.objects.all()
     serializer_class = SampleSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name']
